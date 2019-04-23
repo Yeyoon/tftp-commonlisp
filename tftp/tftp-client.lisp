8095794c76879f628f5d1a8e050a98fe3b8d1446
Yeyoon <Yeyoon@users.noreply.github.com>
2019-04-23 16:46:50 +0800

    new inplement

--- tftp/tftp-client.lisp
+++ tftp/tftp-client.lisp
@@ -5,152 +5,106 @@
   )
 
 (in-package :tftp-moment)
-;; the message struct
-(defstruct message
-  (len 0)
-  data)
 
 
-(defconstant +sbuf+
-  (make-message
-   :len 0
-   :data (make-array 512 :element-type '(unsigned-byte 8))))
+(defparameter *block-num* 0
+  "The block number of the data or ack")
 
-(defconstant +rbuf+
-  (make-message
-   :len 0
-   :data (make-array 512 :element-type '(unsigned-byte 8))))
+(defun read-block-number (data-buf)
+  (let ((n 0))
+    (setf (ldb (byte 8 8) n) (aref data-buf 2))
+    (setf (ldb (byte 8 0) n) (aref data-buf 3))
+    n))
 
-(defun add-end (e mbuf)
-  (progn
-    (setf (aref (message-data mbuf) (message-len mbuf)) e)
-    (incf (message-len mbuf))))
+(defun read-operater-number (data-buf)
+  (let ((n 0))
+    (setf (ldb (byte 8 8) n) (aref data-buf 0))
+    (setf (ldb (byte 8 0) n) (aref data-buf 1))
+    n))
 
-(defun string-to-vector (strs)
-  (let ((v (make-array 0 :element-type '(unsigned-byte 8) :adjustable t :fill-pointer 0)))
-    (loop for c across strs do
-          (vector-push-extend (char-code c) v))
-    (vector-push-extend 0 v)
-    v))
+(defun process-read-error (data-buf)
+  (let ((n (read-block-number data-buf))
+        (str (binary-data-to-null-string data-buf :start 4)))
+    (format t "ERROR CODE : ~a, ERROR MSG : ~a~%" n str))
+	
+;; Process Read file from Server
+(defun write-value (obj dest)
+  (if (numberp obj)
+      (vector-push-extend obj dest)
+      (if (stringp obj)
+          (progn
+            (loop for c across obj do
+                  (vector-push-extend (char-code c) dest))
+            (vector-push-extend 0 dest))
+          (error "Wrong obj ~a for write-value" obj))))
 
-(defun number-to-vector (n size)
-  (labels ((n-t-v (n size v)
-             (if (<= size 0)
-                 v
-                 (progn
-                   (vector-push-extend (ldb (byte 8
-                                                  (- size 8))
-                                            n)
-                                       v)
-                   (n-t-v n (- size 8) v)))))
-    (let ((v (make-array 0
-                         :element-type '(unsigned-byte 8)
-                         :adjustable t
-                         :fill-pointer 0)))
-      (n-t-v n size v)
-      v)))
+(defun tftp-get (client-socket server-addr server-port filename &optional (mode "octet"))
+  (declare (ignore mode))
+  ;; 1. send RRQ
+  (let ((v (make-array 0 :element-type '(unsigned-byte 8) :fill-pointer 0 :adjustable t)))
+    (write-value 0 v)
+    (write-value 1 v)
+    (write-value filename v)
+    (write-value "octet" v)
 
-
-;; rewrite trans-args-to-mbuf-args
-;; args is a list, but each of it must eval
-;; first, so the element is not a list
-
-(defun trans-args-to-mbuf-args (&rest args)
-  (let ((one (first args))
-        (second (second args)))
-    (when one
-      (print one)
-      (if (stringp one)
-          (cons (string-to-vector one) (apply #'trans-args-to-mbuf-args (cdr args)))
-          (if (numberp one)
-              (cons (number-to-vector one second) (apply #'trans-args-to-mbuf-args (cddr args)))
-              (error "Moment: trans-args-to-mbuf-args unkown object ~a" one))))))
-
-(defun merge-vector-mbuf (mbuf vector)
-  (let ((len (length vector)))
-    (dotimes (i len)
-      (add-end (aref vector i) mbuf))))
-
-(defun merge-vectors-mbuf (mbuf vectors)
-  (dolist (v vectors)
-    (merge-vector-mbuf mbuf v)))
-
-
-
-(defmacro build-tftp-msg (tftp-type mbuf &key file-name
-                                              (file-mode "netascci")
-                                              (seq-number 0)
-                                              data
-                                              errormsg)
-  `(let ((v2
-           (trans-args-to-mbuf-args
-            ,@(case tftp-type
-                (:read `(1 16 ,file-name ,file-mode))
-                (:write `(2 16 ,file-name ,file-mode))
-                (:data `(3 16  ,seq-number 16 ,data))
-                (:ack `(4 16 ,seq-number 16))
-                (:error `(5 16 ,seq-number 16 ,errormsg))))))
-     (merge-vectors-mbuf ,mbuf v2)))
-
-
-
-(defun tftp-data-p (vector-buf)
-  (and (= 0 (aref vector-buf 0))
-       (= 3 (aref vector-buf 1))))
-
-(defun tftp-data-number (vector-buf)
-  (+ (* (aref vector-buf 2) 256)
-     (aref vector-buf 3)))
-
-(defmacro new-ack (ack-num)
-  `(let ((v (make-message :len 0
-                         :data (make-array 4 :element-type '(unsigned-byte 8)))))
-    (build-tftp-msg :ack v :seq-number ,ack-num)
-     (message-data v)))
-
-
-(defun write-n-bytes (stream vector-buf &key (start 0) end)
-  (let ((endbytes (or end (length vector-buf))))
-    (loop for i from start below endbytes do
-          (write-byte (aref vector-buf i) stream))))
-
-
-(defun read-file-from-server (socket srcfilename &optional dstfilename)
-  (let ((target-name (or dstfilename srcfilename)))
-    (with-open-file (stream target-name
-                            :element-type '(unsigned-byte 8)
-                            :direction :output
-                            :if-exists :supersede
-                            :if-does-not-exist :create)
+    (format t "v is ~a, len is ~a ~%" v (length v))
+    
+    (usocket:socket-send client-socket v (length v) :host server-addr :port server-port)
+    (with-open-file (stream filename :direction :output :if-does-not-exist :create :if-exists :supersede)
       (loop
         (multiple-value-bind (recv size remote-host remote-port)
-            (usocket:socket-receive socket (message-data +rbuf+) nil)
-          (declare (ignore recv))
-          (setf (message-len +rbuf+) 0)
-          (when (tftp-data-p (message-data +rbuf+))
-            (write-n-bytes stream (message-data +rbuf+) :start 4 :end size)
-            (format t "recv : buf ~a, size ~a" (message-data +rbuf+) size)
-            (usocket:socket-send socket (new-ack (+ 1 (tftp-data-number (message-data +rbuf+)))) 4 :host remote-host :port remote-port)
-            (format t "send ack number ~a" (tftp-data-number (message-data +rbuf+)))
-              (if (< size 512)
-                  ;; we have done read just break
-                  (return)))
-          (when (not (tftp-data-p (message-data +rbuf+)))
-              (format t "is not data message buf is ~a" (message-data +rbuf+)))
-          )))))
+            (usocket:socket-receive client-socket nil 520)
+          (let ((result (handle-get-message recv size stream)))
+            (when result
+              (format t "result is : ~a~%" result))
+            (if result
+                (usocket:socket-send client-socket result (length result) :host remote-host :port remote-port)
+                (return))
 
-(defun create-client-socket ()
-  ;; random port
-  (usocket:socket-connect nil nil
-                          :protocol :datagram
-                          :timeout 10))
+            (when (< size 512) (return))))))
 
-(defun read-file-from-server% (operation filename)
-  (let ((client (create-client-socket)))
-    (declare (ignore operation))
-    (setf (message-len +sbuf+) 0)
-    (build-tftp-msg :read +sbuf+ :file-name filename  :file-mode "octet")
-    (usocket:socket-send client (message-data +sbuf+) (message-len +sbuf+) :port 69 :host "127.0.0.1")
-    (format t "has send ")
-    (read-file-from-server client filename)
-    (usocket:socket-close client)))
+    (usocket:socket-close client-socket)))
+
+
+(defun handle-get-message (recv size stream)
+  (let ((mtype (read-operater-number recv))
+        (block-num (read-block-number recv)))
+    (if (= 5 mtype)
+        ;; process error msg
+        (progn (process-read-error recv) nil)
+        (if (= 3 mtype)
+            (progn
+              (when (= block-num *block-num*)
+                (write-sequence recv stream :start 4 :end size)
+                (incf *block-num*))
+              (let ((ack (make-array 0 :element-type '(unsigned-byte 8) :fill-pointer 0 :adjustable t)))
+                (write-value 0 ack)
+                (write-value 4 ack)
+                (write-value (ldb (byte 8 8) *block-num*) ack)
+                (write-value (ldb (byte 8 0) *block-num*) ack)
+                ack))
+            (format t "get can not process message type ~a" mtype)))))
+
+(defun split-string-with-del (string del)
+  (loop for i = 0 then (+ j 1)
+        as j = (position del string :start i)
+        collect (subseq string i j)
+        while j))
+
+
+(defun run ()
+  (let ((host "127.0.0.1")
+        (port 69)
+        (client-socket (usocket:socket-connect nil nil :protocol :datagram)))
+    (when (not client-socket)
+      (format t "socket error")
+      (return-from run))
+    
+    (format t "> ")
+    (let* ((user-input (read-line))
+           (user-input2 (string-trim " " user-input))
+           (user-list (split-string-with-del user-input2 #\space)))
+      (when (= 3 (length user-list))
+        (setf host (third user-list))
+        (tftp-get client-socket host port (second user-list)))
+      (format t "process get file ~a done. ~%" (second user-list)))))
